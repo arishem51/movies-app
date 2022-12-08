fa595e8f3cd4e65ae1348a3819fb11b77d8cd43b
Arishemm <arishem51@gmail.com>
2022-12-09 04:10:30 +0700

    chore: gh pages

--- src/hook/useDynamicColumn.ts
+++ src/hook/useDynamicColumn.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+// Hack column of Antd List for responsive :>
+
 export const useDynamicColumn = () => {
   const [column, setColumn] = useState(10);
 
